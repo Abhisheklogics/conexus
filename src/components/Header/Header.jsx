ab0db82627ac848a613c65116882183fcb295162
Abhishek Kumar <166478718+Abhisheklogics@users.noreply.github.com>
2025-02-04 02:02:13 +0530

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -20,7 +20,7 @@ export default function Header() {
        </ul>
 <div  style={{backgroundColor:'#222222'}} className=" 
   rounded-lg 2xl:mt-[590px] 2xl:left-[550px] 2xl:block 
-flex gap-4 md:gap-6 bg-[#3E3E3E] p-2 rounded-lg md:h-[80px] md:p-0  w-fit  md:w-[640px] z-50   md:fixed  rounded-lg md:mt-[500px] md:left-[400px] md:block w-full flex justify-center items-center mt-16">
+flex gap-4 md:gap-6 bg-[#3E3E3E] p-2 rounded-lg md:h-[80px] md:p-0  w-fit  md:w-[640px] z-50   md:fixed  rounded-lg md:mt-[440px] md:left-[400px] md:block w-full flex justify-center items-center mt-16">
 
 <ul style={{backgroundColor:'#3E3E3E'}} className=" overflow-x-scroll md:overflow-hidden md:w-[580px] gap-2 flex mt-10 p-2 md:flex md:h-[60px] md:left-10 md:mt-[2px] md:items-center md:gap-4 md:relative md:top-2 rounded-lg text-white     md:px-2">
 

a1b80577436040839c12ea6c93fed5c0df344fdf
Abhishek Kumar <166478718+Abhisheklogics@users.noreply.github.com>
2025-02-06 13:11:02 +0530

    Update Room.jsx

--- src/components/Room/Room.jsx
+++ src/components/Room/Room.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams, useLocation } from 'react-router-dom';
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:4000', { autoConnect: false });
+const socket = io('https://conbeckend.onrender.com', { autoConnect: false });
 
 const Room = () => {
     const { roomId } = useParams();

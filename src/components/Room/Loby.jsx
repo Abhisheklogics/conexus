bc611020730bdc9527d35f40fe72638a405b37ab
Abhishek Kumar <166478718+Abhisheklogics@users.noreply.github.com>
2025-02-06 13:31:21 +0530

    Update Loby.jsx

--- src/components/Room/Loby.jsx
+++ src/components/Room/Loby.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { io } from 'socket.io-client';
 
-const socket = io('https://conbeckend.onrender.com', { autoConnect: false });
+const socket = io('https://conbeckend.onrender.com');
 
 const LobbyRoom = () => {
     const [roomId, setRoomId] = useState('');

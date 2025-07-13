3e457997a58e34a8df7a994f6b6d953eae26c52b
abhi.Logics <abhi.kumar252004@gmail.com>
2025-07-14 01:21:51 +0530

    al

--- src/main.jsx
+++ src/main.jsx
@@ -11,15 +11,9 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Route, RouterProvider,createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
-import Contact from './components/Contact/Contact.jsx'
-import SimpleSlider from './components/Papers/Papers.jsx'
-import Home from './components/Home/Home.jsx'
-import Project from './components/Projects/project.jsx'
 
-import Project1 from './components/Project1/Project1.jsx'
-import Project2 from './components/Project2/Project2.jsx'
-import About from './components/About/About.jsx'
-import LobyRoom from './components/Room/Loby.jsx'
+import Home from './components/Home/Home.jsx'
+
 import Room from './components/Room/Room.jsx'
 import VideoChat from './components/Room/Room.jsx'
 const router=createBrowserRouter(
